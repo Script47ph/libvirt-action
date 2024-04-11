20b18ddc7af0393069433802e0f62453b584c400
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-12 02:51:43 +0700

    CI: Fix minor v8

--- scripts/action.sh
+++ scripts/action.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DRONE_SSH_BIN="drone-ssh"
 DRONE_SSH_ARG="--ssh-key '$INPUT_KEY' -H $INPUT_HOST -p $INPUT_PORT -u $INPUT_USERNAME --script"
 cd scripts
 sh -c ./$DRONE_SSH_BIN $DRONE_SSH_ARG ./installer.sh
