c94d9faa42bdf2bacaf263b524df54fa7674e080
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-12 02:58:04 +0700

    CI: Fix minor v10

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-DRONE_SSH_URL="https://github.com/appleboy/drone-ssh/releases/download/v1.7.4/drone-ssh-1.7.4-linux-amd64"
-DRONE_SSH_BIN="drone-ssh"
-
-get_drone() {
-    curl -fL --retry 3 --keepalive-time 2 $DRONE_SSH_URL -o $DRONE_SSH_BIN
-    chmod +x $DRONE_SSH_BIN
-}
-
 install_libvirt() {
     #check dependency
     os_check=$(grep ^ID= /etc/os-release | cut -d'"' -f2)
@@ -26,5 +17,4 @@ install_libvirt() {
 }
 
 # Main
-get_drone
 install_libvirt
