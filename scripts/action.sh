5889a7efefcb4ee1fc39a76f6a78ab4b60ab1b5a
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-12 02:58:57 +0700

    CI: Fix minor v11

--- scripts/action.sh
+++ scripts/action.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DRONE_SSH_URL="https://github.com/appleboy/drone-ssh/releases/download/v1.7.4/drone-ssh-1.7.4-linux-amd64"
 DRONE_SSH_BIN="drone-ssh"
-DRONE_SSH_ARG="--ssh-key '$INPUT_KEY' -H $INPUT_HOST -p $INPUT_PORT -u $INPUT_USERNAME --script"
+DRONE_SSH_ARG="--ssh-key '$INPUT_KEY' -H $INPUT_HOST -p $INPUT_PORT -u $INPUT_USERNAME"
 
 get_drone() {
     curl -fL --retry 3 --keepalive-time 2 $DRONE_SSH_URL -o $DRONE_SSH_BIN
@@ -9,4 +9,4 @@ get_drone() {
 }
 cd scripts
 get_drone
-sh -c ./$DRONE_SSH_BIN $DRONE_SSH_ARG pwd
+sh -c ./$DRONE_SSH_BIN $DRONE_SSH_ARG --script pwd
