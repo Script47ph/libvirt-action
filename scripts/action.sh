a4cffc3a4684117f7f63973f0b0b6566f5ec9e3c
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-12 21:54:01 +0700

    CI: Testing

--- scripts/action.sh
+++ scripts/action.sh
@@ -1,12 +1,92 @@
-#!/bin/bash
-DRONE_SSH_URL="https://github.com/appleboy/drone-ssh/releases/download/v1.7.4/drone-ssh-1.7.4-linux-amd64"
-DRONE_SSH_BIN="drone-ssh"
-DRONE_SSH_ARG="--ssh-key '$INPUT_KEY' -H $INPUT_HOST -p $INPUT_PORT -u $INPUT_USERNAME"
-
-get_drone() {
-    curl -fL --retry 3 --keepalive-time 2 $DRONE_SSH_URL -o $DRONE_SSH_BIN
-    chmod +x $DRONE_SSH_BIN
+install_libvirt() {
+    #check dependency
+    os_check=$(grep ^ID= /etc/os-release | cut -d'"' -f2)
+    if [[ $os_check =~ ^(centos|redhat|rhel|rocky|alma)$ ]]
+    then
+        sudo yum update -y
+        if rpm -q libvirt &>/dev/null
+        then 
+            echo "libvirt is installed."
+        else 
+            sudo yum install qemu-kvm libvirt -y
+        fi
+    else
+        echo "your os not supported yet."
+    fi
 }
-cd scripts
-get_drone
-sh -c ./$DRONE_SSH_BIN $DRONE_SSH_ARG --script pwd
+install_terraform () {
+    if [ -n "$(which terraform 2>/dev/null)" ]
+    then
+        echo "terraform is installed"
+    else
+        sudo yum install -y yum-utils
+        sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+        sudo yum -y install terraform
+    fi
+}
+install_geniso() {
+    if [ -n "$(which mkisofs 2>/dev/null)" ]
+    then
+        echo "genisoimage is installed"
+    else
+        sudo yum install -y genisoimage
+    fi
+}
+config_libvirt() {
+    if groups|grep libvirt &>/dev/null && groups|grep kvm &>/dev/null
+    then
+        echo user exist in libvirt and kvm groups! skipped..
+    else
+        sudo usermod -aG libvirt $USER
+        sudo usermod -aG kvm $USER
+        newgrp libvirt
+    fi
+    sudo sed -i "s,#unix_sock_group,unix_sock_group,g" /etc/libvirt/libvirtd.conf
+    sudo sed -i "s,#unix_sock_rw_perms,unix_sock_rw_perms,g" /etc/libvirt/libvirtd.conf
+    if sudo grep ^user /etc/libvirt/qemu.conf &>/dev/null
+    then
+        echo user and group exists! skipped..
+    else
+        echo "user = \"$(whoami)\"" | sudo tee -a /etc/libvirt/qemu.conf
+        echo 'group = "libvirt"' | sudo tee -a /etc/libvirt/qemu.conf
+    fi
+    mkdir -p $HOME/.config/libvirt/
+    echo 'uri_default = "qemu:///system"' > $HOME/.config/libvirt/libvirt.conf
+    sudo systemctl restart libvirtd.service
+}
+config_pool() {
+    items=($POOL)
+    for i in "${items[@]}"
+    do
+        if virsh pool-list --name|grep $i &>/dev/null
+        then
+            echo pool $i exist! skipped..
+        else
+            mkdir -p $HOME/data/$i
+            virsh pool-define-as --name $i --type dir --target $HOME/data/$i
+            virsh pool-start $i
+            virsh pool-autostart $i
+            virsh pool-info $i
+        fi
+    done
+}
+config_net() {
+    cat <<EOF> cluster-network.tf
+provider "libvirt" {
+    uri = "qemu:///system"
+}
+
+terraform {
+    required_version = ">= 0.13"
+    required_providers {
+    libvirt = {
+        source  = "dmacvicar/libvirt"
+        version = "0.7.1"
+    }
+    }
+}
+
+$NETWORK_RAW
+    
+EOF
+}
